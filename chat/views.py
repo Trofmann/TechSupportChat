45c0dedebdd128e18aabb48a6090e408f32c98c0
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-05 20:40:07 +0300

    Добавлена передача данных в шаблон

--- chat/views.py
+++ chat/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def message_list(request):
-    return render(request, 'chat/message_list.html', {})
+    return render(request, 'chat/message_list.html', context={"letter": 123})
