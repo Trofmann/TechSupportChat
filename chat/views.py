0aa2d937a5cc0765a84c998790d7e8cb7c26979f
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-06 14:16:01 +0300

    Добавлена отправка сообщения на сервер

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .forms import MessageForm
 from .models import Message
 
 
 def message_list(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+        #return HttpResponse("<h2>{0}</h2>".format(text))
+        print(text)
+
     messages = Message.objects.all()
-    return render(request, 'chat/message_list.html', {'messages': messages})
+    message_form = MessageForm()
+    return render(request, 'chat/message_list.html', {'messages': messages, 'form': message_form})
