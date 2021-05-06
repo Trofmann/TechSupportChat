e6271d83715b9ff72558603cd753ff08d202e7de
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-06 09:35:21 +0300

    Добавлен вывод всех сообщений

--- chat/views.py
+++ chat/views.py
@@ -5,4 +5,4 @@ from .models import Message
 
 def message_list(request):
     messages = Message.objects.all()
-    return render(request, 'chat/message_list.html', {'messages':messages})
+    return render(request, 'chat/message_list.html', {'messages': messages})
