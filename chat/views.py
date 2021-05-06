f18c0ad4a53ec4d68a8d3e6d54b879629441cd00
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-06 14:30:34 +0300

    Отправка и вывод сообщений с датой отправки

--- chat/views.py
+++ chat/views.py
@@ -1,15 +1,16 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from .forms import MessageForm
 from .models import Message
+from django.contrib.auth.models import User
 
 
 def message_list(request):
     if request.method == "POST":
-        text = request.POST.get("text")
-        #return HttpResponse("<h2>{0}</h2>".format(text))
-        print(text)
+        new_message_text = request.POST.get("text")
+        print(new_message_text)
+        new_message = Message(text=new_message_text, author=User.objects.get(username='user'))
+        new_message.send()
 
     messages = Message.objects.all()
     message_form = MessageForm()
