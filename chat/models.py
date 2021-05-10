24cf0ae1c0b117864c1dd2951a793d53f603bd59
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-10 12:19:48 +0300

    Добавлена расширенная модель пользователя с типом пользователя (менеджер или клиент)

--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,20 @@
-# coding=utf-8
-from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
 
 
+class ChatUser(models.Model):
+    """Chat user class"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # type - manager or client
+    type = models.CharField(max_length=7)
+
+
 class Message(models.Model):
-    """ Сообщение"""
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    """ Message class"""
+    author = models.ManyToManyField(ChatUser, related_name="author")
     text = models.CharField(max_length=300)
+    recipient = models.ManyToManyField(ChatUser, related_name="recipient")
     sent_date = models.DateTimeField(blank=True, null=True)
 
     def send(self):
