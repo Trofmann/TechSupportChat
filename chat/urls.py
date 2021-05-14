2564fb318a5f01a494afdf41cf83b83c9941c45b
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-14 17:01:04 +0300

    Добавлена форма авторизации

--- chat/urls.py
+++ chat/urls.py
@@ -10,6 +10,6 @@ from django.contrib.auth import views as auth_views
 from .views import MessageCreateView
 
 urlpatterns = [
-    url(r'^1213', MessageCreateView.as_view(), name='message_list'),
+    url(r'^messages', MessageCreateView.as_view(), name='message_list'),
     url(r'^', auth_views.login, {'template_name': 'chat/login.html'}, name='login')
 ]
