f3d26bc5992559438ebd008a32718ac3a7ec7a47
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-18 15:16:14 +0300

    Обновлена форма

--- chat/forms.py
+++ chat/forms.py
@@ -14,7 +14,8 @@ from models import Message
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
-        exclude = ()
+        # exclude = ()
+        exclude = ['author']
 
     def __init__(self, *args, **kwargs):
         super(MessageForm, self).__init__(*args, **kwargs)
--- chat/views.py
+++ chat/views.py
@@ -20,24 +20,8 @@ class MessageCreateView(CreateView):
     def post(self, request, *args, **kwargs):
         message_form = MessageForm(request.POST)
         if message_form.is_valid():
-            # Достаем из формы текст и автора сообщения
-            # new_message_text = message_form.cleaned_data['text']
-            # new_message_author = message_form.cleaned_data['author']
-            # # Создаем объект сообщения и сохраняем его
-            # new_message = Message(text=new_message_text, author=new_message_author)
-            message_form.object = message_form.save()
+            message_form.object = message_form.save(commit=False)
             message_form.object.author = request.user
             message_form.object.save()
-            # new_message.save()
         context = {'form': MessageForm(), 'messages': Message.objects.filter(author=request.user)}
         return render(request, 'chat/message_list.html', context)
-
-# class MessageCreateView(CreateView):
-#     template_name = 'chat/message_list.html'
-#     form_class = MessageForm
-#
-#     def form_valid(self, form):
-#         self.object = form.save()
-#         self.object.save()
-#         context = {'form': MessageForm(), 'messages': Message.objects.all()}
-#         return render('chat/message_list.html', context)
