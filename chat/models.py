1b83f44f19e904cacdd902194c1fc0ec63798c85
Trofmann <trofimovevgeniy9@gmail.com>
2021-05-18 17:00:57 +0300

    Начата работа над моделью диалога

--- chat/models.py
+++ chat/models.py
@@ -41,11 +41,60 @@ class Message(models.Model):
         return self.text
 
 
-class Chat(models.Model):
-    """Класс чата"""
-    # Клиент
-    client = models.OneToOneField(
+class Dialog(models.Model):
+    """Класс диалога"""
+    # Клиент - владелец диалога
+    owner = models.OneToOneField(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        verbose_name='Клиент',
+        verbose_name='Клиент'
     )
+
+    operator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name='Клиент'
+    )
+
+    messages = models.ForeignKey(
+        Message,
+        on_delete=models.CASCADE,
+        verbose_name='Сообщение'
+    )
+
+
+# class Chat(models.Model):
+#     """Класс чата"""
+#     # Клиент
+#     client = models.OneToOneField(
+#         settings.AUTH_USER_MODEL,
+#         on_delete=models.CASCADE,
+#         verbose_name='Клиент',
+#     )
+#
+#     # Добавить get_absolute_url
+#
+#
+# class Question(models.Model):
+#     """Класс вопроса"""
+#     # Чат, в который входит вопрос
+#     chat = models.ForeignKey(
+#         Chat,
+#         on_delete=models.CASCADE,
+#         verbose_name='Чат'
+#     )
+#
+#     # Взят ли менеджером вопрос в работу
+#     is_taken_to_work = models.BooleanField(
+#         verbose_name='Вопрос взят в работу',
+#         default=False
+#     )
+#
+#     # Решён ли вопрос
+#     is_solved = models.BooleanField(
+#         verbose_name='Вопрос решён',
+#         default=False
+#     )
+#
+#     # Дата начала вопроса
+#     #start_date =
